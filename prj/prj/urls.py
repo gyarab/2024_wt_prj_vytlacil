4328d172d9b6260e7497b18fd200101780505b8c
Vojtech Vytlacil <vojtechvytlacil@gmail.com>
2025-03-14 09:17:15 +0100

    skoola

--- prj/prj/urls.py
+++ prj/prj/urls.py
@@ -20,7 +20,8 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('one', TemplateView.as_view(template_name ="main/one.html")),
-    path('two', TemplateView.as_view(template_name ="main/two.html"))
-
+    path('', TemplateView.as_view(template_name='main/homepage.html')),
+    path('one', TemplateView.as_view(template_name='main/one.html')),
+    path('two', TemplateView.as_view(template_name='main/two.html')),
+    
 ]
