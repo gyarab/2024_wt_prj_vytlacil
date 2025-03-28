a7cb760736c71684ae3afc46ba6132e97259ce01
Vojtech Vytlacil <vojtechvytlacil@gmail.com>
2025-03-28 09:00:41 +0100

    skola

--- prj/main/admin.py
+++ prj/main/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Movie
 
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ["id","name","year"]
+
+
+
+
+admin.site.register(Movie, MovieAdmin)
 # Register your models here.
--- prj/main/models.py
+++ prj/main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+class Movie(models.Model):
+    name = models.CharField(max_length=300)
+    year = models.IntegerField(blank=True, null=True)
+    def __str__(self):
+     return f"{self.name}({self.year})"
 # Create your models here.
